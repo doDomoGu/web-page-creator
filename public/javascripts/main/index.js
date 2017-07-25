dfe2aba2aeb3ba7054f9a1b966b35eddd1508a19
doDomoGu <kaka_71936410@qq.com>
2017-07-25 16:32:43 +0800

    vue 学习

--- public/javascripts/main/index.js
+++ public/javascripts/main/index.js
@@ -4,3 +4,64 @@ var app = new Vue({
         message: 'Hello Vue!'
     }
 })
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: '页面加载于222 ' + new Date()
+    }
+})
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: false
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: '学习 JavaScript' },
+            { text: '学习 Vue' },
+            { text: '整个牛项目' }
+        ]
+    }
+})
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js2222!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Hello Vue!',
+        message2: 'Hello Vue!'
+    }
+})
+
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            { id: 0, text: '蔬菜' },
+            { id: 1, text: '奶酪' },
+            { id: 2, text: '随便其他什么人吃的东西' }
+        ]
+    }
+})
