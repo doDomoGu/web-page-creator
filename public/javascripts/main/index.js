882fd41e8c5bf4a9144465c8327b0fa5d4caf569
doDomoGu <kaka_71936410@qq.com>
2017-08-04 14:26:44 +0800

    调试 axios 调用后台接口

--- public/javascripts/main/index.js
+++ public/javascripts/main/index.js
@@ -1,74 +1,34 @@
-Vue.component('child', {
-    // camelCase in JavaScript
-    props: ['parentMsg'],
-    template: '<span>{{ parentMsg }}</span>'
-})
+import axios from 'axios';
+/*
 
 
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
+Vue.prototype.$ajax = axios;*/
+
+axios.get('http://api.web-page.com/users')
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (response) {
+        console.log(response);
+    });
+/*
+new Vue({
+    el:'#test',
+    data:{data:""},
+    beforeCreate:function(){
+        var url="http://api.web-page.com/users";
+        var _self=this;
+        $.get(url,function(data){
+            console.log(data);
+
+            //_self.data=eval("(" + data +")");
+        })
+        /!*
+        this.$http.get(url).then(function(data){
+            var json=data.body;
+            this.data=eval("(" + json +")");
+        },function(response){
+            console.info(response);
+        })*!/
     }
-})
-
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: '页面加载于222 ' + new Date()
-    }
-})
-var app3 = new Vue({
-    el: '#app-3',
-    data: {
-        seen: false
-    }
-})
-
-var app4 = new Vue({
-    el: '#app-4',
-    data: {
-        todos: [
-            { text: '学习 JavaScript' },
-            { text: '学习 Vue' },
-            { text: '整个牛项目' }
-        ]
-    }
-})
-
-var app5 = new Vue({
-    el: '#app-5',
-    data: {
-        message: 'Hello Vue.js2222!'
-    },
-    methods: {
-        reverseMessage: function () {
-            this.message = this.message.split('').reverse().join('')
-        }
-    }
-})
-
-
-var app6 = new Vue({
-    el: '#app-6',
-    data: {
-        message: 'Hello Vue!',
-        message2: 'Hello Vue!'
-    }
-})
-
-
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
-})
-var app7 = new Vue({
-    el: '#app-7',
-    data: {
-        groceryList: [
-            { id: 0, text: '蔬菜' },
-            { id: 1, text: '奶酪' },
-            { id: 2, text: '随便其他什么人吃的东西' }
-        ]
-    }
-})
+});*/
